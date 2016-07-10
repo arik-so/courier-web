d741d9ddd1d3beb443e6a9eb5c071267098d7314
Michael <perk.michael@t-online.de>
2016-07-11 01:40:55 +0200

    update1

---
+++ resources/js/creative.js
@@ -0,0 +1,43 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    });
+
+    // Initialize and Configure Scroll Reveal Animation
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 1000,
+        scale: 0.3,
+        distance: '0px'
+    }, 200);
+    sr.reveal('.sr-list', {
+        duration: 600,
+        delay: 1000
+    });
+    sr.reveal('.sr-small-icons', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 300);
+
+})(jQuery); // End of use strict
